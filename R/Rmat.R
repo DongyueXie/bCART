ad923f4b4640fb9d83eb5fd569f13c033cf6e0f8
Dongyue Xie <dongyxie@gmail.com>
2019-06-24 20:11:35 -0500

    precision issue

--- R/Rmat.R
+++ R/Rmat.R
@@ -3,6 +3,6 @@
 
 Rmat = function(p){
   Q=gramschmidt(matrix(rnorm(p^2),nrow=p))$Q
-  if(det(Q)==-1){idx=sample(1:p,2);Q[idx,]=Q[c(idx[2],idx[1]),]}
+  if(det(Q)<0){idx=sample(1:p,2);Q[c(idx[1],idx[2]),]=Q[c(idx[2],idx[1]),]}
   return(Q)
 }
