63959f01eb1e6f00885d5a1d63f481fd29084dcd
Dongyue Xie <dongyxie@gmail.com>
2019-09-25 00:31:17 -0500

    CV version

---
+++ R/pBARTr_CV.R
@@ -0,0 +1,13 @@
+#' @title pBART cross-validation
+#' @param ks,powers,bases,ntrees,rs,p_split ks: a vector of k; so on
+#' @param nfold number of folds
+#' @export
+
+
+
+pBARTr_CV = function(x,y,ks=c(0.25,0.5,1,2,3),ntrees=c(50,100),
+                     powers=c(2,3,5,10),bases=c(0.5,0.8,0.95),
+                     rs=c(2,3,5,10,20),p_split,nfold=5){
+
+  1
+}
