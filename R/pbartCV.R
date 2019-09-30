184d6f0d7b2ac8d1f9ff73c3a10331b0a7002741
Dongyue Xie <dongyxie@gmail.com>
2019-09-30 14:13:25 -0500

    use less memory

--- R/pbartCV.R
+++ R/pbartCV.R
@@ -3,7 +3,7 @@
 #'@export
 
 pbartCV = function(x,y,x.test,y.test,ntrees=c(50,200),ks=c(0.5,2,5),powers = c(2,3,5,10,nrow(x)),
-                   k_folds=5,verbose=F,isparallel=T){
+                   k_folds=5,verbose=F){
   n_combns = length(ntrees)*length(ks)*length(powers)
   results = matrix(nrow=n_combns,ncol=4)
   #create folds
@@ -24,27 +24,17 @@ pbartCV = function(x,y,x.test,y.test,ntrees=c(50,200),ks=c(0.5,2,5),powers = c(2
         idx = idx + 1
         if(verbose & idx%%5==0){print(idx)}
 
-        if(isparallel){
-          cv_AUC = mclapply(folds,function(z){
-            x_train = x[-z,]
-            y_train = y[-z]
-            x_test = x[z,]
-            y_test = y[z]
-            fit = quiet(pbart(x_train,y_train,x_test,ntree=it,k=jk,power=kp))
-            Test_AUC = AUC(y_test,as.numeric(colSums(pnorm(fit$yhat.test))/nrow(fit$yhat.test)>0.5))
-            Test_AUC
-          },mc.cores = k_folds)
-        }else{
+
           cv_AUC = lapply(folds,function(z){
             x_train = x[-z,]
             y_train = y[-z]
             x_test = x[z,]
             y_test = y[z]
-            fit = quiet(pbart(x_train,y_train,x_test,ntree=it,k=jk,power=kp))
-            Test_AUC = AUC(y_test,as.numeric(colSums(pnorm(fit$yhat.test))/nrow(fit$yhat.test)>0.5))
+            prob.test = quiet(pbart(x_train,y_train,x_test,ntree=it,k=jk,power=kp,nkeeptrain=0,nkeeptreedraws=0))$prob.test.mean
+            Test_AUC = AUC(y_test,as.numeric(prob.test>0.5))
             Test_AUC
           })
-        }
+
         cv_AUC = mean(unlist(cv_AUC))
         results[idx,] = c(cv_AUC,it,jk,kp)
       }
@@ -53,9 +43,9 @@ pbartCV = function(x,y,x.test,y.test,ntrees=c(50,200),ks=c(0.5,2,5),powers = c(2
 
   best_params = results[which.max(results[,1]),]
 
-  fit = quiet(pbart(x,y,x.test,ntree=best_params[2],k=best_params[3],power=best_params[4]))
+  prob.test = quiet(pbart(x,y,x.test,ntree=best_params[2],k=best_params[3],power=best_params[4],nkeeptrain=0,nkeeptreedraws=0))$prob.test.mean
 
-  ypred = as.numeric(colSums(pnorm(fit$yhat.test))/nrow(fit$yhat.test)>0.5)
+  ypred = as.numeric(prob.test>0.5)
   Test_AUC = AUC(y.test,ypred)
 
   return(list(ypred=ypred,Test_AUC=Test_AUC,best_params=best_params))
